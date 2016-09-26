fb14c7bee9dfca0d99d3a2d81f4122f99cb7b14d
Julie Park <jspark721@gmail.com>
2016-09-26 14:47:26 -0500

    Added break time to controller

--- app/scripts/controllers/TimerCtrl.js
+++ app/scripts/controllers/TimerCtrl.js
@@ -2,18 +2,46 @@
     function TimerCtrl($scope, $interval){
         $scope.startTime = 1500000;
         $scope.format= 'mm:ss'; //minutes and seconds
-        $scope.startTimer= function(){
+        $scope.onBreak = false;
+        $scope.breakTime = 300000;
         
-            $interval(function(){
+        var timer;
+        $scope.startTimer= function(){
+            timer = $interval(function(){
                 $scope.startTime -=1000;
-            },1000);
+                
+                // when timer hits 0:00, timer stops and break timer starts
+                if($scope.startTime == 0) {
+                    $interval.cancel(timer);
+                    $scope.onBreak = true;
+                    
+                    $scope.breakTimer = function() {
+                        $interval(function(){
+                            $scope.breakTime -=1000;
+                            
+                            if($scope.breakTime == 0) {
+                                $scope.onBreak = false;
+                            }
+                        },1000);
+                    }
+                }
+            }, 1000);
+            
+            $scope.stop = function(){
+                $interval.cancel(timer);
+            }
         };
+        
         $scope.resetTimer = function(){
             $scope.startTime= 1500000;
         }
-    }
+        
+    };
     
     angular
         .module('timerApp')
         .controller('TimerCtrl', TimerCtrl);
 })();
+
+timerApp.constant('START_TIME', 15000000);
+timerApp.constant('')
