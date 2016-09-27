81e788a865d1f2886114b9c7451561a0caa08da6
Julie Park <jspark721@gmail.com>
2016-09-27 01:28:40 -0500

    Added long break timer

--- app/scripts/controllers/TimerCtrl.js
+++ app/scripts/controllers/TimerCtrl.js
@@ -9,32 +9,32 @@
         $scope.longBreak = MY_TIMERS.LONG_BREAK_TIME;
         $scope.completedWorkSessions= 0;
         
+        
         var timer;
         var breakTimer;
         
         $scope.startTimer= function(){
             timer = $interval(function(){
                 $scope.startTime -=1000;
-                $scope.workSession= false;
                 
                 // when timer hits 0:00, timer stops and break timer starts
                 if($scope.startTime == 0) {
                     $scope.stop();
                     $scope.onBreak = true;
                     
-                    if($scope.onBreak || $scope.onLongBreak) {
+                    if($scope.onBreak) {
                         $scope.setBreakTime();
-                            
-                        if($scope.breakTime == 0) {
+                        
+                        if($scope.breakTime == 0 ){
                             $scope.onBreak = false;
                         }
-                    }
+                    } 
                 }
             }, 1000);
         };
         
         $scope.resetTimer = function(){
-            $scope.startTime= 1500000;
+            $scope.startTime= MY_TIMERS.WORK_SESSION;
         }
         
         $scope.stop = function() {
@@ -51,12 +51,22 @@
              }, 1000);
         }
         
+        $scope.setLongBreak = function(){
+            longBreakTimer= $interval(function() {
+                $scope.longBreak -=1000;
+                
+                if($scope.longBreak == 0) {
+                    $interval.cancel(longBreakTimer);
+                }
+            }, 1000);
+        }
+        
     };
     
     angular
         .module('timerApp')
         .constant('MY_TIMERS', {
-            WORK_SESSION: 15000,
+            WORK_SESSION: 5000,
             BREAK_TIME: 3000,
             LONG_BREAK_TIME: 18000
         })
